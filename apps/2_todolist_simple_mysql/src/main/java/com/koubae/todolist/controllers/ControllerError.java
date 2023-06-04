8ec13da1cb487e86e805bf9c8e40d0d09839bee3
Koubae <58447627+Koubae@users.noreply.github.com>
2023-06-04 09:12:08 +0200

    fIX ERRRO CONTROLLER

--- apps/2_todolist_simple_mysql/src/main/java/com/koubae/todolist/controllers/ControllerError.java
+++ apps/2_todolist_simple_mysql/src/main/java/com/koubae/todolist/controllers/ControllerError.java
@@ -4,30 +4,39 @@ import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 public class ControllerError implements ErrorController {
-    @RequestMapping("/error")
-    public String handleError(HttpServletRequest request) {
-        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-        Exception error = (Exception) request.getAttribute(RequestDispatcher.ERROR_EXCEPTION);
+    private record ResponseError (int status, String url, String error, String description) {}
 
-        String errorMessageTitle = "<h3>Something Went Wrong</h3>";
-        String errorMessageBody = String.format("<p>Error: %s </p>", error);
+    @RequestMapping(value = "/error", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseError handleError(HttpServletRequest request) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        String url = (String) request.getAttribute(RequestDispatcher.ERROR_REQUEST_URI);
+        String errorMessage = (String) request.getAttribute(RequestDispatcher.ERROR_MESSAGE);
+
+        int statusCode = -1;
+        String errorMessageBody = "Something Went Wrong";
         if (status != null) {
-            int statusCode = Integer.parseInt(status.toString());
+            statusCode = Integer.parseInt(status.toString());
             if(statusCode == HttpStatus.NOT_FOUND.value()) {
-                errorMessageTitle = "<h3>Error 404 - Not Found</h3>";
-                errorMessageBody = "<p>What you requested wasn't found!</p>";
+                errorMessageBody = "Error 404 - Not Found";
             }
             else if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
-                errorMessageBody = "<h3>Error 500 - Internal Server Error</h3>";
+                errorMessageBody = "Error 500 - Internal Server Error";
             }
         }
-        return errorMessageTitle + errorMessageBody;
+
+        return new ResponseError(
+                statusCode,
+                url,
+                errorMessageBody,
+                errorMessage
+        );
 
     }
 }
